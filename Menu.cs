d6bcd8b30acea9c958202598f2b0f11bb2791ea5
Alex Zarate <azarate45@gmail.com>
2026-04-16 17:40:02 -0500

    Creación Proyecto.

---
+++ Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3dSemana4
+{
+    internal class Menu
+    {
+        public void Execute()
+        {
+
+        }
+    }
+}
